ae346e801db2d56cae1d9c4f7beecb1ca9b6d1ee
Douglas Toledo <douglas22toledo@gmail.com>
2019-09-02 23:27:43 -0300

    Half of module 3 completed

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,29 @@
 import { Router } from 'express';
+import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middlewares/auth';
+import multerConfig from './config/multer';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.put('/users', authMiddleware, UserController.update);
 
 routes.post('/sessions', SessionController.store);
+
+routes.get('/providers', ProviderController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+routes.post('/appointments', authMiddleware, AppointmentController.store);
+routes.get('/appointments', authMiddleware, AppointmentController.index);
+
+routes.get('/schedules', authMiddleware, ScheduleController.index);
 
 export default routes;
